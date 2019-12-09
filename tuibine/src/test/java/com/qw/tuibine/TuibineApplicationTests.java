1024a267386bbeaa668329340787941f24c70e1e
Eclair-F <56910441+Eclair-F@users.noreply.github.com>
2019-12-10 00:04:32 +0800

    cloud turbine

---
+++ tuibine/src/test/java/com/qw/tuibine/TuibineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qw.tuibine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuibineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
